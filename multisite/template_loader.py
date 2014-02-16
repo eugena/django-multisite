a6adddfc6b84c33a41fe024061c5a46be07db412
Eugena A. Mikhailikova <eugena@inbox.ru>
2014-02-16 18:00:42 +0600

    adaptation

--- multisite/template_loader.py
+++ multisite/template_loader.py
@@ -8,26 +8,26 @@ from django.utils._os import safe_join
 
 class Loader(FilesystemLoader):
     def get_template_sources(self, template_name, template_dirs=None):
-        if not template_dirs:
-            template_dirs = settings.TEMPLATE_DIRS
+        if settings.SITE_ID > 1:
+            if not template_dirs:
+                template_dirs = settings.TEMPLATE_DIRS
 
-        domain = Site.objects.get_current().domain
-        default_dir = getattr(settings, 'MULTISITE_DEFAULT_TEMPLATE_DIR', 'default')
+            domain = Site.objects.get_current().domain
+            default_dir = getattr(settings, 'MULTISITE_DEFAULT_TEMPLATE_DIR', 'default')
+            new_template_dirs = []
+            for template_dir in template_dirs:
+                new_template_dirs.append(safe_join(template_dir, domain))
+                if default_dir:
+                    new_template_dirs.append(safe_join(template_dir, default_dir))
+            for template_dir in new_template_dirs:
+                try:
+                    yield safe_join(template_dir, template_name)
 
-        new_template_dirs = []
-        for template_dir in template_dirs:
-            new_template_dirs.append(safe_join(template_dir, domain))
-            if default_dir:
-                new_template_dirs.append(safe_join(template_dir, default_dir))
-
-        for template_dir in new_template_dirs:
-            try:
-                yield safe_join(template_dir, template_name)
-            except UnicodeDecodeError:
-                # The template dir name was a bytestring that wasn't valid UTF-8.
-                raise
-            except ValueError:
-                # The joined path was located outside of this particular
-                # template_dir (it might be inside another one, so this isn't
-                # fatal).
-                pass
+                except UnicodeDecodeError:
+                    # The template dir name was a bytestring that wasn't valid UTF-8.
+                    raise
+                except ValueError:
+                    # The joined path was located outside of this particular
+                    # template_dir (it might be inside another one, so this isn't
+                    # fatal).
+                    pass
